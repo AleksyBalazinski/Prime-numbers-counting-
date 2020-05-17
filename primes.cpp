1ee4d5e498c4e6e784cdad01d0d1f147bdd0fb91
AleksyBalazinski <54417806+AleksyBalazinski@users.noreply.github.com>
2020-05-17 21:51:45 +0200

    Update primes.cpp

--- primes.cpp
+++ primes.cpp
@@ -1,4 +1,4 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include <iostream>
 #include <vector> 
 #include <cmath>
@@ -32,7 +32,7 @@ int main()
 	cin >> n;
 	divisor = 0;
 	std::vector<int> primes;
-	auto start = high_resolution_clock::now(); //start measuring how long it takes to execute this loop
+	auto start1 = high_resolution_clock::now();
 	for (int k = 1; k <= n; k++)
 	{
 		divisor = 0;
@@ -42,20 +42,21 @@ int main()
 			{
 				divisor++;
 			}
+			if (divisor >= 3) break;
 		}
 		if (divisor <= 2 && divisor != 1)
 		{
 			primes.push_back(k);
 		}
 	}
-	auto stop = high_resolution_clock::now(); //here we stop the experiment
+	auto stop1 = high_resolution_clock::now();
 	cout << "\nFollowing primes were found in range between 1 and " << n << "\n";
 	cout << "That is " << primes.size() << " in total\n";
 	for (auto elem : primes) cout << elem << "\t";
-	double estimate = n / log(n);
-	cout << "\nNumber of primes determined by Gauss's formula x/ln(x) is: " << estimate << "\n";
-	cout << "That's " << estimate / static_cast<double>(primes.size()) * 100 << "% accurate\n";
-	auto duration = duration_cast<microseconds>(stop - start); //counts in microseconds
-	cout << "It took " << duration.count()/1000 << "ms"; //result in miliseconds (ms)
+	cout << "biggest prime is: " << primes[primes.size() - 1] << "\n";
+	double estimate1 = n / log(n);
+	cout << "\nNumber of primes determined by Gauss's formula x/ln(x) is: " << estimate1 << "\n";
+	cout << "That's " << estimate1 / static_cast<double>(primes.size()) * 100 << "% accurate\n";
+	auto duration2 = duration_cast<microseconds>(stop1 - start1); //counts in microseconds
+	cout << "Now it took " << duration2.count() / 1000000 << "s"; //result in seconds (ms)
 }
-
